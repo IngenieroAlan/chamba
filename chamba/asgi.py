88e0c429474cd69d11512e948a443ee233e4684d
IngenieroAlan <brandon.rod182@gmail.com>
2023-03-16 16:58:55 -0700

    Primer Commit

---
+++ chamba/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for chamba project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chamba.settings')
+
+application = get_asgi_application()
---
+++ paginaApp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PaginaappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'paginaApp'
